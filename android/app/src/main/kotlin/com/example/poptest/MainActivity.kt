542f6f687563648b46144b7143a05c120368ed37
devansh <devansh2330@users.noreply.github.com>
2023-06-26 12:02:10 +0530

    temporary commit

---
+++ android/app/src/main/kotlin/com/example/poptest/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.poptest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:poptest/popup.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: GestureDetector(
+          child: Text('Open Popup'),
+          onTap: () {
+            showModalBottomSheet(
+              context: context,
+              builder: (BuildContext context) {
+                return BottomPopup();
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
